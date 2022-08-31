15cf843e3b413b12c150ef2cddc335a92e496116
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-08-31 19:31:18 +0900

    jest stack test

---
+++ unit-basic/src/stack/stack.js
@@ -0,0 +1,32 @@
+class Stack {
+    constructor() {
+        this.array = [];
+    }
+
+    push(value) {
+        this.array.push(value);
+    }
+
+    pop() {
+        if (this.array.length > 0) {
+            return this.array.pop();
+        } else {
+            throw Error('Stack is empty');
+        }
+    }
+
+    peek() {
+        if (this.array.length > 0) {
+            return this.array[this.size() - 1];
+        } else {
+            throw Error('Stack is empty');
+        }
+
+    }
+
+    size() {
+        return this.array.length;
+    }
+}
+
+module.exports = Stack;
---
+++ unit-basic/src/stack/test/stack.test.js
@@ -0,0 +1,46 @@
+const Stack = require('../stack.js');
+
+describe('Stack class', () => {
+    let stack;
+    beforeEach(() => {
+        stack = new Stack();
+    })
+
+    it('is created empty', () => {
+        expect(stack.size()).toEqual(0);
+    });
+
+    it('allows to push item', () => {
+        stack.push(1);
+        expect(stack.size()).toBe(1);
+    });
+
+    describe('pop', () => {
+        it('throw an error if stack is empty', () => {
+            expect(() => {
+                stack.pop()
+            }).toThrow('Stack is empty');
+        });
+        it('returns the last pushed item and removes it from the stack', () => {
+            stack.push(1);
+            stack.push(2);
+
+            expect(stack.pop()).toBe(2);
+            expect(stack.array.length).toBe(1);
+        });
+
+    })
+
+    describe('peek', () => {
+        it('throw an error if stack is empty', () => {
+            expect(() => {
+                stack.peek();
+            }).toThrow('Stack is empty');
+        });
+        it('returns the last pushed item but keeps it in the stack', () => {
+            stack.push(1);
+            expect(stack.peek()).toBe(1);
+        });
+    });
+
+});
